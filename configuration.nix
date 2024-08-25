77134898081860ccc73a534772c0407ee24890c3
mmkaram <mahdykaram@outlook.com>
2024-08-24 22:31:30 -0700

    fourth commit

---
+++ configuration.nix
@@ -0,0 +1,312 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+# commands to run
+# gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+{ config, lib, pkgs, inputs, ... }:
+{
+
+  # enable expiremental nix features
+   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  imports =
+    [
+      ./hardware-configuration.nix
+	inputs.home-manager.nixosModules.default
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "Rocinante"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  # sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dd0k = {
+    isNormalUser = true;
+    description = "dD0k";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      firefox
+      zoom-us
+    ];
+  };
+
+
+## Text editing
+programs.neovim = {
+  enable = true;
+  defaultEditor = false;
+};
+services.kanata = {
+  enable = true;
+};
+
+## Gaming
+programs.steam = {
+	enable = true;
+	remotePlay.openFirewall = true;
+	dedicatedServer.openFirewall = true;
+};
+## Insecure Packages
+nixpkgs.config.permittedInsecurePackages = [
+];
+	# bash default?
+        # programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        users.defaultUserShell = pkgs.bash;
+	programs.zsh.enable = true;
+	
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+	nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+  vim
+spotify-player
+      	# vim_configurable 
+# My stuff
+# Rust
+# run: rustup deafult stable 
+lldb
+linuxPackages_latest.perf
+rustup
+rustc
+rustfmt
+rust-analyzer
+cargo
+clippy
+du-dust
+libgcc
+clang
+# performance
+btop
+htop
+hotspot
+# Python
+python3
+python311Packages.pyenchant
+python311Packages.numpy
+stdenv.cc.cc.lib
+python311Packages.matplotlib
+libglibutil
+ffmpeg
+libGL
+libGLU
+# prog general
+zip
+kitty
+lazygit
+lazydocker
+blesh
+atuin
+oh-my-posh
+zsh-powerlevel10k
+docker
+atuin
+scc
+eza
+bat
+speedtest-go
+imagemagick_light
+gh
+vscode
+zed-editor
+neovim
+drawio
+# formatting
+ntfs3g
+dosfstools
+gparted
+# gaming
+steam
+# office
+kanata
+apostrophe
+clipman
+obsidian
+evolution
+evolution-ews
+zotero
+ranger
+wl-clipboard
+ticktick
+# art
+prusa-slicer
+gimp
+vlc
+# socials
+yt-dlp
+tuir
+# other/misc
+autojump
+screenfetch
+fastfetch
+calibre
+qbittorrent
+whois
+cmatrix
+fzf
+atuin
+zsh
+git
+microsoft-edge
+spotify
+discord
+element-desktop
+	wget
+        micro
+
+
+feh
+tmux
+epr
+
+# games
+bastet
+ninvaders
+nsnake
+# libre-office
+libreoffice-qt
+hunspell
+hunspellDicts.uk_UA
+hunspellDicts.th_TH
+# gnome
+gnomeExtensions.hibernate-status-button
+];
+
+
+home-manager = {
+	# also pass inputs to home-manager modules
+	extraSpecialArgs = { inherit inputs; };
+	
+	backupFileExtension = "backup";
+
+	users = {
+		"dd0k" = import ./home.nix;
+	};
+};
+
+  services.kanata = {
+    # enable = true;
+    keyboards.default = {
+      config = ''
+		(defsrc
+		  grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+		  tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+		  caps a    s    d    f    g    h    j    k    l    ;    '    ret
+		  lsft z    x    c    v    b    n    m    ,    .    /    rsft
+		  lctl lmet lalt           spc            ralt prtsc rctl
+		)
+		(deflayer qwerty
+		  grv  1    2    3    4    5    6    7    8    9    0    -    =    caps
+		  tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+		  bspc a    s    d    f    g    h    j    k    l    ;    '    ret
+		  lsft z    x    c    v    b    n    m    ,    .    /    rsft
+		  lctl lmet lalt           spc            ralt prtsc rctl
+		)
+      '';
+    };
+  };
+
+fonts.packages = with pkgs; [
+	(nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+	# Enable Docker
+	virtualisation.docker.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.qemuGuest.enable = true;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+
+
+}
---
+++ home.nix
@@ -0,0 +1,213 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "dd0k";
+  home.homeDirectory = "/home/dd0k";
+  home.stateVersion = "24.05";
+
+  home.packages = [
+    # Add other packages here if needed
+  ];
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      gc="git commit -a -m";
+      gP="git push";
+      gp="git pull";
+      ga="git add .";
+      gitgraph="git log --online --graph --decorate -all";
+      lgit="lazygit";
+      ldoc="lazydocker";
+      t="tmux";
+      icat="kitten icat";
+      salah="bash ~/prog/salah";
+
+      tetris="bastet";
+      space_invaders="nivaders";
+      snake="nsnake";
+
+	nix-rebuild="sudo nixos-rebuild switch --flake /etc/nixos#default";
+        nix-update="cd /etc/nixos && sudo nix flake update";
+        nix-delete-builds="sudo nix-collect-garbage --delete-older-than";
+
+      # TODO:
+      # jump doesn't work because sourcing?
+      jump="autojump";
+      cls="clear";
+      r="ranger";
+      py="python3";
+      el="eza -al";
+      sp="spotify_player";
+      ff="fastfetch";
+      n="nvim";
+      sn="sudo nvim";
+
+      hibernate="systemctl hibernate";
+    };
+    initExtra = ''
+      export PROG="/home/dd0k/prog"
+      export NIX="/etc/nixos"
+      # alias f="nvim $(fzf -m --preview="bat --color=always {}")"
+      '';
+  };
+
+  programs.oh-my-posh = {
+    enable = true;
+    enableBashIntegration = true;
+    useTheme = "material";
+  };
+
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableBashIntegration = true;
+    theme = "Dracula";
+    keybindings = {
+      "f11" = "toggle_fullscreen";
+      "ctrl+shift+left" = "previous_window";
+      "ctrl+shift+right" = "next_window";
+    };
+  };
+
+  programs.ranger = {
+    enable = true;
+    mappings = { 
+      dD = "delete";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "mmkaram";
+    userEmail = "mahdykaram@outlook.com";
+    extraConfig.init = {
+      defaultBranch = "main";
+      safe.directory = "/etc/nixos";
+      push.autoSetupRemote = true;
+    };
+  };
+
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    plugins = with pkgs.vimPlugins; [ vim-airline jedi-vim nerdtree ];
+    settings = { ignorecase = true; };
+    extraConfig = ''
+      set mouse=a
+    '';
+  };
+
+programs.neovim = {
+  enable = true;
+  plugins = with pkgs.vimPlugins; [
+    mason-nvim
+    mason-lspconfig-nvim
+    nvim-lspconfig
+    telescope-nvim
+    plenary-nvim  # Required dependency for Telescope
+    nvim-cmp
+    cmp-nvim-lsp
+    cmp-buffer
+    cmp-path
+    cmp-cmdline
+    cmp-cmdline-history
+
+    {
+      plugin = tokyonight-nvim;
+      config = "colorscheme tokyonight-night";
+    }
+  ];
+extraConfig = ''
+  lua << EOF
+    -- Setup Mason
+    require('mason').setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        },
+        border = "rounded",
+      },
+    })
+
+    -- Setup Mason-LSPConfig
+    require('mason-lspconfig').setup({
+      -- List your desired LSPs here
+      ensure_installed = {}, 
+      automatic_installation = true,
+    })
+
+    -- Setup Telescope
+    local telescope = require('telescope')
+    telescope.setup{}
+
+    -- Keybindings for Telescope
+    vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true, silent = true })
+
+    -- Keybindings for Mason UI
+    vim.api.nvim_set_keymap('n', '<leader>m', '<cmd>Mason<cr>', { noremap = true, silent = true })
+  EOF
+'';
+};
+
+
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      set -g prefix C-x
+      unbind C-z
+      bind C-x send-prefix
+    '';
+  };
+
+  programs.spotify-player = {
+    enable = true;
+    settings = { 
+      client_id = "d5d01a282d5d475f8edf35dd5aab6b55";
+      enable_notify = false;
+      device = {
+        volume = 100;
+        name = "term";
+        normalization = false;
+      };
+    };
+  };
+
+  programs.atuin = { 
+    enable = true;
+  };
+
+  home.file = {
+    # Your dotfiles here
+  };
+
+  home.sessionVariables = {
+    # Your environment variables here
+  };
+
+  programs.home-manager.enable = true;
+  
+  
+  # Dark mode
+    dconf.settings = {
+      "org/gnome/desktop/background" = {
+        picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
+      };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
+    };
+}
+
