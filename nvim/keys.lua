2d2b47d756acb9f43e644fb350c2b877f879a31c
mmkaram <mahdykaram@outlook.com>
2024-11-13 20:58:53 -0500

    messed with some keymaps

--- nvim/keys.lua
+++ nvim/keys.lua
@@ -1,3 +1,21 @@
+local opts = { noremap = true, silent = true }
+function map_key(mode, lhs, rhs, opts)
+    -- Set default options
+    local default_opts = { noremap = true, silent = true }
+
+    -- If opts is provided, merge it with default_opts
+    if opts then
+        for k, v in pairs(opts) do
+            default_opts[k] = v
+        end
+    end
+
+    -- Set the keymap
+    vim.api.nvim_set_keymap(mode, lhs, rhs, default_opts)
+end
+
+vim.cmd('set number relativenumber')
+
 vim.g.mapleader = " "
 	-- Horizontal split
 	vim.api.nvim_set_keymap('n', '<Leader>sh', ':split<CR>', { noremap = true, silent = true })
@@ -9,7 +27,7 @@ vim.g.mapleader = " "
 	vim.api.nvim_set_keymap('n', '<Leader>nv', ':vnew<CR>', { noremap = true, silent = true })
 
 	-- fullscreen
-	vim.api.nvim_set_keymap('n', '<Leader>f', ':ZenMode<CR>', {noremap = true, silent = true})
+	map_key('n', '<Leader>f', ':ZenMode<CR>')
 
 	-- pane movement
 	vim.api.nvim_set_keymap('n', '<c-k>', ':wincmd k<CR>', { noremap = true, silent = true })
@@ -17,8 +35,20 @@ vim.g.mapleader = " "
 	vim.api.nvim_set_keymap('n', '<c-h>', ':wincmd h<CR>',{ noremap = true, silent = true })
 	vim.api.nvim_set_keymap('n', '<c-l>', ':wincmd l<CR>', { noremap = true, silent = true })
 
+	-- TODO: broken
+	-- Resize panes vertically
+	map_key("n", "<C-S-k>", ":resize +2<CR>")
+	map_key("n", "<C-S-j>", ":resize -2<CR>")
+	-- Resize panes horizontally
+	map_key("n", "<C-S-h>", ":vertical resize -2<CR>")
+	map_key("n", "<C-S-l>", ":vertical resize +2<CR>")
+
+	-- Tabs
+	map_key("n", "<tab>", ":bnext<CR>")
+	map_key("n", "<s-tab>", ":bprevious<CR>")
+
 	-- close buffers
 	vim.api.nvim_set_keymap('n', '<c-w>', ':bd<CR>', { noremap = true, silent = true})
-	
+
 	-- preview hunk
 	vim.api.nvim_set_keymap('n', '<Leader>lh', ':Git preview_hunk<CR>', { noremap = true, silent = true })
