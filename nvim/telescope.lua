6c59501fbe47106c90ec006421b34b368faa449c
mmkaram <mahdykaram@outlook.com>
2024-12-22 20:09:08 +0200

    keybind

--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -13,6 +13,7 @@ vim.g.mapleader = " "
 	vim.api.nvim_set_keymap('n', '<leader>tF', ':Telescope file_browser<CR>', { noremap = true, silent = true })
 	vim.api.nvim_set_keymap('n', '<leader>tg', ':Telescope grep_string<CR>', { noremap = true, silent = true })
 	vim.api.nvim_set_keymap('n', '<leader>tb', ':Telescope lsp_dynamic_workspace_symbols<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>tv', ':Telescope lsp_document_symbols<CR>', { desc = 'Show document symbols', noremap = true, silent = true })
 
 	-- Key mapping to open the file browser
 	vim.api.nvim_set_keymap('n', '<leader>te', ':Telescope file_browser<CR>', { noremap = true, silent = true })
