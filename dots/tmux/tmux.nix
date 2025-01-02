482c31355a2f272ad78c6241ab9c764e58e49e80
mmkaram <mahdykaram@outlook.com>
2025-01-02 21:52:50 +0200

    fixed tmux bugs

--- dots/tmux/tmux.nix
+++ dots/tmux/tmux.nix
@@ -20,10 +20,6 @@
     set -g base-index 1 # Set base index for windows to 1
     set -g pane-base-index 1 # Set base index for panes to 1
 
-    # Optional: Uncomment to set default command/shell
-    # set-option -g default-command "${pkgs.zsh}/bin/zsh"
-    # set-option -g default-shell "${pkgs.zsh}/bin/zsh"
-
     # Set status bar colors
     set -g status-bg blue
     set -g status-fg black
@@ -32,18 +28,18 @@
     set -s escape-time 0
 
     # Key mappings for pane splitting and navigation
-    bind -n C-% split-window -h   # Ctrl + % to split a pane horizontally
-    bind -n C-\ split-window -v    # Ctrl + \ to split a pane vertically
+    bind-key -n C-% split-window -h      # Ctrl + 5 to split pane horizontally
+    bind-key -n C-'\' split-window -v    # Ctrl + \ to split pane vertically
 
     # Navigation between panes using vim-like keys (hjkl)
-    bind -n C-x h select-pane -L   # Ctrl + x then h to move left
-    bind -n C-x j select-pane -D   # Ctrl + x then j to move down
-    bind -n C-x k select-pane -U   # Ctrl + x then k to move up
-    bind -n C-x l select-pane -R   # Ctrl + x then l to move right
+    bind-key h select-pane -L            # h to move left
+    bind-key j select-pane -D            # j to move down
+    bind-key k select-pane -U            # k to move up
+    bind-key l select-pane -R            # l to move right
 
     # Key mappings for window navigation
-    bind -n C-n next-window         # Ctrl + n for next window
-    bind -n C-p previous-window     # Ctrl + p for previous window
+    bind-key -n C-n next-window          # Ctrl + n for next window
+    bind-key -n C-p previous-window      # Ctrl + p for previous window
 
   '';
 }
