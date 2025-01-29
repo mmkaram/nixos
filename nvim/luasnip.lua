8af0cf11b5f4a13c455ac1292c7a65d78b1b1d00
mmkaram <mahdykaram@outlook.com>
2025-01-29 16:55:03 -0500

    better luasnip keybinds

--- nvim/luasnip.lua
+++ nvim/luasnip.lua
@@ -2,6 +2,8 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local extras = require("luasnip.extras")
+local r = extras.rep
 
 -- ALL SNIPS
 ls.add_snippets("all", {
@@ -17,6 +19,31 @@ local jsset = {
 		t("console.log("),
 		i(1),
 		t(")"),
+	}),
+	s("rfc", {
+		t({
+			"import React from 'react';",
+			"",
+			"const ",
+		}),
+		i(1, "ComponentName"),
+		t({
+			" = () => {",
+			"  return (",
+			"    <div>",
+			"      ",
+		}),
+		i(2, "content"),
+		t({
+			"",
+			"    </div>",
+			"  );",
+			"};",
+			"",
+			"export default ",
+		}),
+		r(1),
+		t(";"),
 	}),
 }
 
@@ -35,19 +62,13 @@ ls.add_snippets("lua", {
 
 -- KEYMAPS
 
-vim.keymap.set({ "i" }, "<C-q>", function()
+vim.keymap.set({ "i", "s" }, "<A-k>", function()
 	if ls.expand_or_jumpable() then
 		ls.expand_or_jump()
 	end
 end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<tab>", function()
-	if ls.expand_or_jumpable() then
-		ls.expand_or_jump()
-	end
-end, { silent = true })
-
-vim.keymap.set({ "i", "s" }, "<S-tab>", function()
+vim.keymap.set({ "i", "s" }, "<A-j>", function()
 	if ls.jumpable(-1) then
 		ls.jump(-1)
 	end
