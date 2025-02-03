e0e0aaa79f348da5ddd7ded38b973a4eb5aeadf2
mmkaram <mahdykaram@outlook.com>
2025-02-03 15:20:40 -0500

    update notifs on nvim

--- nvim/notify.lua
+++ nvim/notify.lua
@@ -1,8 +1,9 @@
 math.randomseed(os.time())
-local intros = {'Welcome, Paul.', 'Lead them to Paradise', 'I Must not Fear. Fear is the Mind Killer', 'Silence', 'There is no escape — we pay for the violence of our ancestors'}
+-- local intros = {'Welcome, Paul.', 'Lead them to Paradise', 'I Must not Fear. Fear is the Mind Killer', 'Silence', 'There is no escape — we pay for the violence of our ancestors'}
+local intros = { "Welcome, Paul." }
 local function pickRandomString(list)
-    local index = math.random(#list)
-    return list[index]
+	local index = math.random(#list)
+	return list[index]
 end
 
 require("notify")(pickRandomString(intros))
