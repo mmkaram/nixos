8f9a19602e76ebf48307f95f87263e466c878656
mmkaram <mahdykaram@outlook.com>
2025-02-07 01:28:38 -0500

    added warning

--- nvim/obsidian.lua
+++ nvim/obsidian.lua
@@ -1,4 +1,6 @@
 require("obsidian").setup({
+	-- TODO: This poops itself if it can't find the folder
+	-- will break on a fresh install if this vault is not right here
 	workspaces = {
 		{
 			name = "world",
