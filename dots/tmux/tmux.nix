708262f278fa1333e189a13ecf5da0763233d9ad
mmkaram <mahdykaram@outlook.com>
2025-03-05 13:58:09 -0500

    added tmux nav keybinds

--- dots/tmux/tmux.nix
+++ dots/tmux/tmux.nix
@@ -48,6 +48,17 @@
     bind-key -n C-n next-window          # Ctrl + n for next window
     bind-key -n C-p previous-window      # Ctrl + p for previous window
 
+    # Bind Alt + [1-9] to switch to windows 1-9
+    bind-key -n M-1 select-window -t 1
+    bind-key -n M-2 select-window -t 2
+    bind-key -n M-3 select-window -t 3
+    bind-key -n M-4 select-window -t 4
+    bind-key -n M-5 select-window -t 5
+    bind-key -n M-6 select-window -t 6
+    bind-key -n M-7 select-window -t 7
+    bind-key -n M-8 select-window -t 8
+    bind-key -n M-9 select-window -t 9
+
     set -ga terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
 
     # TODO: Add ctrl + [ for copy paste mode
