06ca3d25eb3f6280e3e98455b126a450ad3db96b
mmkaram <mahdykaram@outlook.com>
2025-03-26 08:02:09 -0400

    cleanup v2

--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -1,4 +1,4 @@
-local telescope = require("telescope").setup({
+require("telescope").setup({
 	pickers = {
 		find_files = {
 			hidden = true,
@@ -9,27 +9,40 @@ local telescope = require("telescope").setup({
 -- Set leader key
 vim.g.mapleader = " "
 -- Mapping to open Telescope
-vim.api.nvim_set_keymap("n", "<leader>tt", ":Telescope<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tt", ":Telescope<CR>", { noremap = true, silent = true, desc = "List telescope" })
 -- Additional mappings for various Telescope functions
-vim.api.nvim_set_keymap("n", "<leader>ti", ":Telescope treesitter<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope buffers<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tr", ":Telescope lsp_references<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>td", ":Telescope lsp_definitions<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
+vim.keymap.set(
+	"n",
+	"<leader>ti",
+	":Telescope treesitter<CR>",
+	{ noremap = true, silent = true, desc = "List treesitter" }
+)
+vim.keymap.set("n", "<C-p>", ":Telescope buffers<CR>", { noremap = true, silent = true, desc = "List buffers" })
+vim.keymap.set(
+	"n",
+	"<leader>tr",
+	":Telescope lsp_references<CR>",
+	{ noremap = true, silent = true, desc = "LSP references" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>td",
+	":Telescope lsp_definitions<CR>",
+	{ noremap = true, silent = true, desc = "LSP definitions" }
+)
+vim.keymap.set(
 	"n",
 	"<leader>tf",
 	":Telescope current_buffer_fuzzy_find<CR>",
-	{ noremap = true, silent = true }
+	{ noremap = true, silent = true, desc = "Find document string" }
 )
-vim.api.nvim_set_keymap("n", "<leader>tF", ":Telescope file_browser<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tg", ":Telescope grep_string<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>tb",
 	":Telescope lsp_dynamic_workspace_symbols<CR>",
-	{ noremap = true, silent = true }
+	{ noremap = true, silent = true, desc = "Show workspace symbols" }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>tv",
 	":Telescope lsp_document_symbols<CR>",
@@ -37,34 +50,25 @@ vim.api.nvim_set_keymap(
 )
 
 -- Key mapping to open the file browser
-vim.api.nvim_set_keymap("n", "<leader>te", ":Telescope file_browser<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-S-o>", ":Telescope file_browser<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-o>", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.set.set("n", "<leader>te", ":Telescope file_browser<CR>", { noremap = true, silent = true, desc = "List files" })
+vim.keymap.set("n", "<C-o>", ":Telescope find_files<CR>", { noremap = true, silent = true, desc = "Find files" })
 
 -- Marks
-vim.api.nvim_set_keymap("n", "<leader>tm", ":Telescope marks<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tm", ":Telescope marks<CR>", { noremap = true, silent = true, desc = "List marks" })
 
 -- Search
-vim.api.nvim_set_keymap("n", "<C-s>", ":Telescope live_grep<CR>", { noremap = true, silent = true })
+vim.keymap.set(
+	"n",
+	"<C-s>",
+	":Telescope live_grep<CR>",
+	{ noremap = true, silent = true, desc = "Find workspace string" }
+)
 
 -- Problems/diagnostics
-vim.api.nvim_set_keymap("n", "<leader>tp", "<cmd>Telescope diagnostics<cr>", { desc = "List diagnostics" })
+vim.keymap.set("n", "<leader>tp", "<cmd>Telescope diagnostics<cr>", { desc = "List diagnostics" })
 
 -- registers
-vim.api.nvim_set_keymap("n", '<leader>t"', "<cmd>Telescope registers<cr>", { desc = "List registers" })
+vim.keymap.set("n", '<leader>t"', "<cmd>Telescope registers<cr>", { desc = "List registers" })
 
 -- -- LSP rename
 vim.keymap.set("n", "<leader>tn", vim.lsp.buf.rename, { desc = "LSP rename" })
--- require('telescope').setup({
---   extensions = {
---     ['ui-select'] = {
---       require('telescope.themes').get_dropdown()
---     }
---   }
--- })
---
--- require('telescope').load_extension('ui-select')
---
--- vim.keymap.set('n', '<leader>tn', function()
---   return ":IncRename " .. vim.fn.expand("<cword>")
--- end, { expr = true })
