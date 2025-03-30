0898a809a4f103ed2e22ef7b2fa203921151cfc7
mmkaram <mahdykaram@outlook.com>
2025-03-30 00:48:29 -0400

    neo-tree working

--- packages/neo-tree-nvim/package.nix
+++ packages/neo-tree-nvim/package.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 pkgs.vimUtils.buildVimPlugin {
-  name = "git-dashboard-nvim";
+  name = "neo-tree.nvim";
   version = "2025-03-27";
   src = pkgs.fetchFromGitHub {
     owner = "nvim-neo-tree";
@@ -8,4 +8,5 @@ pkgs.vimUtils.buildVimPlugin {
     rev = "69f798bf9493b84df660ac3c6b2fc03e23956d25";
     sha256 = "sha256-ny5D878LbmaMn9djwM9yRnW4xpdXf6ye7wa7JSnLy3s=";
   };
+  doCheck = false;
 }
