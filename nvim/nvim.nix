9431f486017d19b70ef29a149465ddffbcf09fe7
mmkaram <mahdykaram@outlook.com>
2025-03-30 00:04:27 -0400

    added neotree

--- nvim/nvim.nix
+++ nvim/nvim.nix
@@ -61,7 +61,7 @@ in {
 
     {
       plugin = neo-tree-nvim;
-      config = toLua "require('neo-tree').setup({window={mappings = {['l'] = 'toggle_node',},},sources = {'filesystem', 'document_symbols'},}) ";
+      config = toLuaFile ./neotree.lua;
     }
 
     cmp_luasnip
