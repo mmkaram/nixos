d86ddf57ad8f1c790e1debb6c17f5adb006a8315
mmkaram <mahdykaram@outlook.com>
2025-04-23 02:27:19 -0400

    stopped notify from auto running

--- nvim/notify.lua
+++ nvim/notify.lua
@@ -1,11 +1,11 @@
 math.randomseed(os.time())
 -- local intros = {'Welcome, Paul.', 'Lead them to Paradise', 'I Must not Fear. Fear is the Mind Killer', 'Silence', 'There is no escape — we pay for the violence of our ancestors'}
-local intros = { "Welcome, Paul." }
-local function pickRandomString(list)
-	local index = math.random(#list)
-	return list[index]
-end
+-- local intros = { "Welcome, Paul." }
+-- local function pickRandomString(list)
+-- 	local index = math.random(#list)
+-- 	return list[index]
+-- end
 
-require("notify")(pickRandomString(intros))
+-- require("notify")(pickRandomString(intros))
 vim.notify = require("notify")
 require("telescope").load_extension("notify")
