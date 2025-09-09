2763dedafbcf817bc36534a03c8d055e3206b184
mmkaram <mahdykaram@outlook.com>
2025-09-09 14:24:57 +0300

    lua formatting for tags

--- nvim/tags.lua
+++ nvim/tags.lua
@@ -1,13 +1,13 @@
-require('nvim-ts-autotag').setup({
-  opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = false -- Auto close on trailing </
-  },
-  per_filetype = {
-    ["html"] = {
-      enable_close = false
-    }
-  }
+require("nvim-ts-autotag").setup({
+	opts = {
+		-- Defaults
+		enable_close = true, -- Auto close tags
+		enable_rename = true, -- Auto rename pairs of tags
+		enable_close_on_slash = false, -- Auto close on trailing </
+	},
+	per_filetype = {
+		["html"] = {
+			enable_close = false,
+		},
+	},
 })
