3a5736a82ed4c659d6c32f3cce7db5516c488056
mmkaram <mahdykaram@outlook.com>
2025-09-09 16:25:51 +0300

    removed lspconfig

--- nvim/lsp.lua
+++ nvim/lsp.lua
@@ -1,40 +1,3 @@
-local lspconfig = require("lspconfig")
-local util = require("lspconfig.util")
-lspconfig.tailwindcss.setup({})
--- Require utils for merge
-local function merge(a, b)
-	for k, v in pairs(b) do
-		a[k] = v
-	end
-end
-
-local function on_attach(_, bufnr)
-	vim.lsp.inlay_hint.enable(true)
-	vim.keymap.set("n", "<Space>ta", vim.lsp.buf.signature_help, { buffer = bufnr, noremap = true, silent = true })
-end
-
-local settings = {
-
-	-- TODO: Elixir, racket?
-	rust_analyzer = {
-		cmd = { "rust-analyzer" },
-		settings = {
-			["rust-analyzer"] = {
-				cargo = { loadOutDirsFromCheck = true, features = "full" },
-				procMacro = { enable = true },
-				expressionAdjustmentHints = { enable = true },
-				lifetimeElisionHints = { enable = true },
-				inlayHints = {
-					enable = true,
-					showParameterNames = true,
-					parameterHintsPrefix = "<- ",
-					otherHintsPrefix = "=> ",
-				},
-			},
-		},
-	},
-}
-
 vim.lsp.config("arduino_language_server", {
 	cmd = {
 		"arduino-language-server",
@@ -60,19 +23,12 @@ vim.lsp.enable({
 	"clangd",
 	"pyright",
 	"ts_ls",
+	"tailwindcss",
 	"arduino_language_server",
+	"rust_analyzer",
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local nvim_lsp = require("lspconfig")
-for server, config in pairs(settings) do
-	local setup_obj = {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
-	merge(setup_obj, config)
-	nvim_lsp[server].setup(setup_obj)
-end
 
 -- Set up nvim-cmp
 local cmp = require("cmp")
