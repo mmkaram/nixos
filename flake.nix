2e1a41a0a12ebc7b615db548fa37314f1e4e643b
mmkaram <mahdykaram@outlook.com>
2025-09-10 14:55:20 +0300

    added prisma-lsp back

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-prisma.url = "github:nixos/nixpkgs/fdd340a071fbb4c10010fb2f8a15467700de2c31";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,13 +25,29 @@
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-prisma,
     agenix,
     nix-alien,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {allowUnfree = true;};
+    };
+
+    pkgsPrisma = import nixpkgs-prisma {
+      inherit system;
+      config = {allowUnfree = true;};
+    };
+  in {
     nixosConfigurations.Rocinante = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
+      specialArgs = {
+        inherit inputs;
+        pkgsPrisma = pkgsPrisma;
+      };
       modules = [
         ./configuration.nix
         agenix.nixosModules.default
