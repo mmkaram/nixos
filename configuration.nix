0accdb10709170990e552e6df3294704aeea9cbb
mmkaram <mahdykaram@outlook.com>
2025-09-14 22:57:40 +0300

    sddm

--- configuration.nix
+++ configuration.nix
@@ -95,7 +95,7 @@
   hardware.graphics.enable = true;
 
   # Enable window manager
-  services.displayManager.gdm.enable = true;
+  services.displayManager.sddm.enable = true;
   programs.hyprlock.enable = true;
   programs.hyprland = {
     enable = true;
