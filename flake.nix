ed0f6db879e4082760f5aae3ba8b3c490d6816a8
mmkaram <mahdykaram@outlook.com>
2025-09-23 22:00:21 +0300

    using unstable hurl

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-old.url = "github:nixos/nixpkgs/813d065adb7d28076c1649a9761777dae5e38108";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,28 +21,16 @@
     {
       self,
       nixpkgs,
-      nixpkgs-old,
       agenix,
       ...
     }@inputs:
     {
-      oldPkgs = final: prev: {
-        hurl =
-          (import nixpkgs-old {
-            system = prev.system;
-            config.allowUnfree = true;
-          }).hurl;
-      };
-
       nixosConfigurations.Rocinante = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = {
           inherit inputs;
         };
         modules = [
-          {
-            nixpkgs.overlays = [ self.oldPkgs ];
-          }
           ./hosts/Rocinante/configuration.nix
           agenix.nixosModules.default
           inputs.home-manager.nixosModules.default
