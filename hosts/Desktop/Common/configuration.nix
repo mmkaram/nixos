34b0073d1ef8907d5b553427029a0e356294a1d8
mmkaram <mahdykaram@outlook.com>
2026-01-09 00:49:05 -0800

    common factor extraction from config files

--- hosts/Desktop/Common/configuration.nix
+++ hosts/Desktop/Common/configuration.nix
@@ -1,14 +1,11 @@
 {
   pkgs,
   inputs,
-  config,
   ...
 }:
 {
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
+  imports = [
+    ../../Common/configuration.nix
   ];
 
   # Enable window manager
@@ -27,20 +24,6 @@
   # Programs
   environment.systemPackages = import ../Common/programs.nix { inherit pkgs inputs; };
 
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Automount
-  services.udisks2.enable = true;
-  services.gvfs.enable = true; # For integration with file managers
-
-  # services
-  services.automatic-timezoned.enable = true;
-  i18n.defaultLocale = "en_US.UTF-8";
-  # Enable openGL
-  hardware.graphics.enable = true;
-
   # Brightness and volume
   programs.light.enable = true;
 
@@ -56,7 +39,6 @@
   };
 
   # Enable sound with pipewire.
-  # sound.enable = true;
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -65,100 +47,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
-    #jack.enable = true;
   };
 
-  ##########################################
-  #  _   _   ____    _____   ____    ____  #
-  # | | | | / ___|  | ____| |  _ \  / ___| #
-  # | | | | \___ \  |  _|   | |_) | \___ \ #
-  # | |_| |  ___) | | |___  |  _ <   ___) |#
-  #  \___/  |____/  |_____| |_| \_\ |____/ #
-  ##########################################
-  # Users
-  users.users.dd0k = {
-    isNormalUser = true;
-    description = "dD0k";
-    hashedPasswordFile = config.age.secrets.password.path;
-    # Groups
-    # esp32: dialout, uucp
-    # wireshark: wireshark
-    # docker: docker
-    # virt-man: libvirtd
-    extraGroups = [
-      "users"
-      "networkmanager"
-      "wheel"
-      "docker"
-      "video"
-      "libvirtd"
-      "dialout"
-      "uucp"
-      "wireshark"
-    ];
-    packages = with pkgs; [
-      firefox
-      zoom-us
-    ];
-  };
-  users.defaultUserShell = pkgs.fish;
-  programs.fish.enable = true;
-  programs.zsh.enable = true;
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    backupFileExtension = "backup";
-    users = {
-      "dd0k" = import (../${config.networking.hostName}/home.nix);
-    };
-  };
-
-  #####################################################################
-  #  ____    ____     ___     ____   ____       _      __  __   ____  #
-  # |  _ \  |  _ \   / _ \   / ___| |  _ \     / \    |  \/  | / ___| #
-  # | |_) | | |_) | | | | | | |  _  | |_) |   / _ \   | |\/| | \___ \ #
-  # |  __/  |  _ <  | |_| | | |_| | |  _ <   / ___ \  | |  | |  ___) |#
-  # |_|     |_| \_\  \___/   \____| |_| \_\ /_/   \_\ |_|  |_| |____/ #
-  #####################################################################
-  # Programs
-  # Enable insecure and unfree packages
-  nixpkgs.config.permittedInsecurePackages = [ ];
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable nix-ld
-  programs.nix-ld.enable = true;
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
-
-  programs.wireshark = {
-    enable = true;
-    package = pkgs.wireshark;
-  };
-
-  services.kanata = import ../../../dots/kanata/kanata.nix { };
-
-  # FONTS
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-  ];
-
-  ##################################################################
-  #  ____    _____   ____   __     __  ___    ____   _____   ____  #
-  # / ___|  | ____| |  _ \  \ \   / / |_ _|  / ___| | ____| / ___| #
-  # \___ \  |  _|   | |_) |  \ \ / /   | |  | |     |  _|   \___ \ #
-  #  ___) | | |___  |  _ <    \ V /    | |  | |___  | |___   ___) |#
-  # |____/  |_____| |_| \_\    \_/    |___|  \____| |_____| |____/ #
-  ##################################################################
   # services
   # ios usb tethering
   services.usbmuxd.enable = true;
-
-  # Enable Docker
-  virtualisation.docker.enable = true;
 
   # Virtualization
   services.qemuGuest.enable = true;
@@ -172,59 +65,23 @@
     };
   };
 
-  # age secrets manager
-  age = import ../../../secrets/age.nix;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # Enable localsend
   programs.localsend.enable = true;
   programs.localsend.openFirewall = true;
 
-  # Enable mullvad vpn
-  services.mullvad-vpn = {
+  programs.steam = {
     enable = true;
-    package = pkgs.mullvad-vpn;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
   };
 
-  # yubikey/yubico
-  services.pcscd.enable = true;
-  services.udev.packages = [ pkgs.yubikey-personalization ];
-
-  security.pam.u2f = {
+  programs.wireshark = {
     enable = true;
-    # control = "required";
-    settings = {
-      interactive = true;
-      cue = true;
-      authFile = "/etc/nixos/secrets/u2f_keys";
-    };
+    package = pkgs.wireshark;
   };
 
-  security.pam.services = {
-    login = {
-      u2fAuth = true;
-      # unixAuth = false;
-    };
-    sudo = {
-      u2fAuth = true;
-      # unixAuth = false;
-    };
-  };
-
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-    settings = {
-      disable-scdaemon = true;
-    };
-  };
+  # Enable nix-ld
+  programs.nix-ld.enable = true;
 
   # Allow 16MB for max perf locked memory
   boot.kernel.sysctl = {
@@ -239,6 +96,4 @@
       options = "--delete-older-than 7d";
     };
   };
-
-  system.stateVersion = "24.11";
 }
