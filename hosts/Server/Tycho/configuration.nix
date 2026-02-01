e10a829052c6a8f360ef0aa008c2cbda3131bf5a
mmkaram <mahdykaram@outlook.com>
2026-02-01 01:09:48 -0800

    update homepage

--- hosts/Server/Tycho/configuration.nix
+++ hosts/Server/Tycho/configuration.nix
@@ -7,6 +7,7 @@
     ./hardware-configuration.nix
     ../../Common/configuration.nix
     ./torrent.nix
+    ./homepage.nix
     # inputs.home-manager.nixosModules.default
   ];
 
@@ -221,86 +222,6 @@
         hash = "sha256-IygCDgwJ+hZ1d39OXuJMrkaxPhVuxSkHy9bWU5NeM/E=";
       };
     };
-  };
-
-  services.homepage-dashboard = {
-    enable = true;
-
-    listenPort = 3002;
-    allowedHosts = "home.mmkaram.dev";
-
-    settings = {
-      title = "Tycho";
-      theme = "dark";
-      color = "slate";
-      headerStyle = "clean";
-      hideVersion = true;
-    };
-
-    services = [
-      {
-        Streaming = [
-          {
-            Jellyfin = {
-              href = "https://jellyfin.mmkaram.dev";
-              description = "Movies & TV";
-            };
-          }
-          {
-            Navidrome = {
-              href = "https://navidrome.mmkaram.dev";
-              description = "Music";
-            };
-          }
-        ];
-      }
-      {
-        Dev = [
-          {
-            Gitea = {
-              href = "https://git.mmkaram.dev";
-              description = "Git hosting";
-            };
-          }
-        ];
-      }
-      {
-        Infra = [
-          {
-            "Uptime Kuma" = {
-              href = "https://status.mmkaram.dev";
-              description = "Monitoring";
-            };
-          }
-          {
-            Atuin = {
-              href = "https://atuin.mmkaram.dev";
-              description = "Shell history sync";
-            };
-          }
-        ];
-      }
-      {
-        Security = [
-          {
-            Vaultwarden = {
-              href = "https://vaultwarden.mmkaram.dev";
-              description = "Password manager";
-            };
-          }
-        ];
-      }
-      {
-        Docs = [
-          {
-            Wiki = {
-              href = "https://wiki.mmkaram.dev";
-              description = "MediaWiki";
-            };
-          }
-        ];
-      }
-    ];
   };
 
   services.cloudflared = {
---
+++ hosts/Server/Tycho/homepage.nix
@@ -0,0 +1,93 @@
+{ ... }:
+{
+  services.homepage-dashboard = {
+    enable = true;
+
+    listenPort = 3002;
+    allowedHosts = "home.mmkaram.dev";
+
+    settings = {
+      title = "Tycho";
+      theme = "dark";
+      color = "slate";
+      headerStyle = "clean";
+      hideVersion = true;
+    };
+
+    services = [
+      {
+        Arr = [
+          {
+            qbit = {
+              href = "https://qbt.mmkaram.dev";
+              description = "Torrenting client";
+            };
+          }
+        ];
+      }
+      {
+        Streaming = [
+          {
+            Jellyfin = {
+              href = "https://jellyfin.mmkaram.dev";
+              description = "Movies & TV";
+            };
+          }
+          {
+            Navidrome = {
+              href = "https://navidrome.mmkaram.dev";
+              description = "Music";
+            };
+          }
+        ];
+      }
+      {
+        Dev = [
+          {
+            Gitea = {
+              href = "https://git.mmkaram.dev";
+              description = "Git hosting";
+            };
+          }
+        ];
+      }
+      {
+        Infra = [
+          {
+            "Uptime Kuma" = {
+              href = "https://status.mmkaram.dev";
+              description = "Monitoring";
+            };
+          }
+          {
+            Atuin = {
+              href = "https://atuin.mmkaram.dev";
+              description = "Shell history sync";
+            };
+          }
+        ];
+      }
+      {
+        Security = [
+          {
+            Vaultwarden = {
+              href = "https://vaultwarden.mmkaram.dev";
+              description = "Password manager";
+            };
+          }
+        ];
+      }
+      {
+        Docs = [
+          {
+            Wiki = {
+              href = "https://wiki.mmkaram.dev";
+              description = "MediaWiki";
+            };
+          }
+        ];
+      }
+    ];
+  };
+
+}
