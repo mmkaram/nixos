f2a606669216fcbaccbc44e9e6ff3888674591f8
mmkaram <mahdykaram@outlook.com>
2026-02-02 09:48:00 -0800

    move wiki to different file

--- hosts/Server/Tycho/configuration.nix
+++ hosts/Server/Tycho/configuration.nix
@@ -6,6 +6,7 @@
   imports = [
     ./hardware-configuration.nix
     ../../Common/configuration.nix
+    ./wiki.nix
     ./torrent.nix
     ./sonarr.nix
     ./radarr.nix
@@ -185,48 +186,6 @@
     enable = true;
     settings = {
       PORT = "3001";
-    };
-  };
-
-  services.mediawiki = {
-    enable = true;
-    name = "Karam Wiki";
-    httpd.virtualHost = {
-      hostName = "wiki.mmkaram.dev";
-      adminAddr = "admin@mmkaram.dev"; # TODO
-      listen = [
-        {
-          ip = "127.0.0.1";
-          port = 8089;
-          ssl = false;
-        }
-      ];
-    };
-
-    database = {
-      type = "mysql";
-      createLocally = true;
-    };
-
-    # Administrator account username is admin.
-    # Set initial password to "cardbotnine" for the account admin.
-    passwordFile = /run/agenix/mediawiki;
-    extraConfig = ''
-      # Disable anonymous editing
-      $wgGroupPermissions['*']['edit'] = false;
-      # Disable new user registration
-      $wgGroupPermissions['*']['createaccount'] = false;
-    '';
-
-    extensions = {
-      # some extensions are included and can enabled by passing null
-      VisualEditor = null;
-
-      # https://www.mediawiki.org/wiki/Extension:TemplateStyles
-      TemplateStyles = pkgs.fetchzip {
-        url = "https://extdist.wmflabs.org/dist/extensions/TemplateStyles-REL1_40-5c3234a.tar.gz";
-        hash = "sha256-IygCDgwJ+hZ1d39OXuJMrkaxPhVuxSkHy9bWU5NeM/E=";
-      };
     };
   };
 
