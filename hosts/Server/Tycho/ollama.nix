455ba342875e757879e729a60988dce34fdc97f4
mmkaram <mahdykaram@outlook.com>
2026-06-12 10:15:05 -0700

    comment out ollama because its breaking

--- hosts/Server/Tycho/ollama.nix
+++ hosts/Server/Tycho/ollama.nix
@@ -1,33 +1,33 @@
 { pkgs, ... }:
 
 {
-  services.ollama = {
-    enable = true;
-    package = pkgs.ollama-cuda;
-
-    host = "0.0.0.0";
-    port = 11434;
-
-    loadModels = [
-      "llama3.1:8b"
-      "deepseek-r1:14b"
-      "qwen3:14b"
-      "qwen2.5:14b"
-    ];
-  };
-
-  services.open-webui = {
-    enable = true;
-
-    host = "127.0.0.1";
-    port = 8081;
-    openFirewall = false;
-
-    environment = {
-      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-      WEBUI_AUTH = "true";
-    };
-
-    stateDir = "/var/lib/open-webui";
-  };
+#   services.ollama = {
+#     enable = true;
+#     package = pkgs.ollama-cuda;
+# 
+#     host = "0.0.0.0";
+#     port = 11434;
+# 
+#     loadModels = [
+#       "llama3.1:8b"
+#       "deepseek-r1:14b"
+#       "qwen3:14b"
+#       "qwen2.5:14b"
+#     ];
+#   };
+# 
+#   services.open-webui = {
+#     enable = true;
+# 
+#     host = "127.0.0.1";
+#     port = 8081;
+#     openFirewall = false;
+# 
+#     environment = {
+#       OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+#       WEBUI_AUTH = "true";
+#     };
+# 
+#     stateDir = "/var/lib/open-webui";
+#   };
 }
