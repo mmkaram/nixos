0555e4af1e34bd0b6622c1dd91082d3dcd84e220
mmkaram <mahdykaram@outlook.com>
2026-08-04 15:17:43 -0700

    beszel formatting

--- hosts/Server/Tycho/beszel.nix
+++ hosts/Server/Tycho/beszel.nix
@@ -1,28 +1,27 @@
-{ ... } :
+{ ... }:
 {
-	services.beszel.hub = {
-		enable = true;
-		environment = {
-			# DISABLE_PASSWORD_AUTH = "true";
-		};
-		host = "0.0.0.0";
-	};
+  services.beszel.hub = {
+    enable = true;
+    environment = {
+      # DISABLE_PASSWORD_AUTH = "true";
+    };
+    host = "0.0.0.0";
+  };
 
-	services.beszel.agent = {
-		enable = true;
-		environment = {
-			SKIP_SYSTEMD = false;
-			KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvq+q3EJkp92NggXIN9sdkk8nhf2iJIT5MLOY6jqoer";
-		};
+  services.beszel.agent = {
+    enable = true;
+    environment = {
+      SKIP_SYSTEMD = false;
+      KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvq+q3EJkp92NggXIN9sdkk8nhf2iJIT5MLOY6jqoer";
+    };
 
-		smartmon ={
-			enable = true;
-			deviceAllow = [
-			"/dev/sda"
-			"/dev/sdb"
-			"/dev/sdc"
-			];
-		};
-	};
+    smartmon = {
+      enable = true;
+      deviceAllow = [
+        "/dev/sda"
+        "/dev/sdb"
+        "/dev/sdc"
+      ];
+    };
+  };
 }
-
