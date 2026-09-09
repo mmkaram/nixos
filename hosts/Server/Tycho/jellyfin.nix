316a2457a7c426b149c74a0370e436d40e73141f
mmkaram <mahdykaram@outlook.com>
2026-09-09 01:36:30 -0700

    cache dir

--- hosts/Server/Tycho/jellyfin.nix
+++ hosts/Server/Tycho/jellyfin.nix
@@ -5,15 +5,8 @@
     enable = true;
     openFirewall = true;
 
-    extraOptions = [
-      "--cachedir=/srv/library/jellyfin-cache"
-    ];
+    cacheDir = "/srv/library/jellyfin-cache";
   };
-
-  systemd.tmpfiles.rules = [
-    "d /srv/library/jellyfin-cache 0750 jellyfin jellyfin -"
-    "d /srv/library/jellyfin-transcodes 0750 jellyfin jellyfin -"
-  ];
 
   environment.systemPackages = with pkgs; [
     python3
