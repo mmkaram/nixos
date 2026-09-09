238a78e05b762005309cee656709df716b6fdeec
mmkaram <mahdykaram@outlook.com>
2026-09-09 01:31:36 -0700

    update jellyfun cache/transcode paths

--- hosts/Server/Tycho/jellyfin.nix
+++ hosts/Server/Tycho/jellyfin.nix
@@ -1,15 +1,24 @@
 { pkgs, ... }:
+
 {
   services.jellyfin = {
     enable = true;
     openFirewall = true;
+
+    extraOptions = [
+      "--cachedir=/srv/library/jellyfin-cache"
+    ];
   };
+
+  systemd.tmpfiles.rules = [
+    "d /srv/library/jellyfin-cache 0750 jellyfin jellyfin -"
+    "d /srv/library/jellyfin-transcodes 0750 jellyfin jellyfin -"
+  ];
+
   environment.systemPackages = with pkgs; [
     python3
-    # jf
     jellyfin
     jellyfin-web
     jellyfin-ffmpeg
   ];
-
 }
